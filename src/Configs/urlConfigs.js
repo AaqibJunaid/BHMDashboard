3887bfade6c513b69051b032fa4a3ca3b53d5b04
AaqibJunaid <43271996+AaqibJunaid@users.noreply.github.com>
2024-03-23 06:52:10 +0000

    Update urlConfigs.js

--- src/Configs/urlConfigs.js
+++ src/Configs/urlConfigs.js
@@ -6,7 +6,7 @@ const shortVideoID='897985033'
 
 // export const nodejsEndpoint = "https://mosquerestapi.glitch.me/"
 export const prayerTimesEndpoint = "https://p2y4v6qkpc.execute-api.eu-west-2.amazonaws.com/prod/prayerdata"
-export const vimeoConfigEndpoint = "https://p2y4v6qkpc.execute-api.eu-west-2.amazonaws.com/prod/vimeoids1"
+export const vimeoConfigEndpoint = "https://p2y4v6qkpc.execute-api.eu-west-2.amazonaws.com/prod/vimeoids"
 export const youtubeEmbed = "https://www.youtube.com/embed/"+youtubeID+"?controls=0&autoplay=1&loop=1&mute=1&playlist="+youtubeID
 export const youtubeMiniEmbed = "https://www.youtube.com/embed/"+youtubeMiniID+"?controls=0&autoplay=1&loop=1&mute=1&playlist="+youtubeMiniID
 // export const mainVideoEmbed = "https://player.vimeo.com/video/"+mainVideoID+"?autoplay=1&loop=1&title=0&byline=0&portrait=0&muted=1&background=1"
